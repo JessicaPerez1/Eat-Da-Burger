4dcfa874105ecc8c7485acb021ab1bd47eaaf340
Jessica Perez <1jessicaperez@gmail.com>
2020-08-07 16:41:04 -0400

    Folder structure and app started

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ("Bacon");
+INSERT INTO burgers (name) VALUES ("Cheese");
+INSERT INTO burgers (name) VALUES ("Veggy");
+INSERT INTO burgers (name) VALUES ("Fish");
+INSERT INTO burgers (name) VALUES ("Exotic");
